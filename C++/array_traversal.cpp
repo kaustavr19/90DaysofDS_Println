5c9fe3f90c774d2f828515610265742138c341bd
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-09 23:58:30 +0530

    array traversal

---
+++ C++/array_traversal.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int arr[8]={9,19,29,39,49,59,69,79};
+    int i;
+
+	for(i=0;i<8;i++){
+			cout<<arr[i]<<endl;
+	}
+}
---
+++ C/array_traversal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	int arr[8]={9,19,29,39,49,59,69,79};
+    int i;
+
+	for(i=0;i<8;i++){
+			printf("%d\n",arr[i]);
+	}
+}
