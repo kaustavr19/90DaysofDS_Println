7adc4071903c0f659d6e5f10b129a71ce8427cb1
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-09 21:36:37 +0530

    array initialisation and declaration

---
+++ C++/array_initialize_declare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+
+    arr[0] = 100;
+    arr[1] = 200;
+    arr[2] = 300;
+    arr[3] = 400;
+    arr[4] = 500;
+
+    cout<< arr[0]<<endl;
+    cout<< arr[1]<<endl;
+    cout<< arr[2]<<endl;
+    cout<< arr[3]<<endl;
+    cout<< arr[4]<<endl;
+
+}
